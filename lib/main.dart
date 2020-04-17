7d2ec8a65870ca099b23b4e9de41542acabcd22c
Eduardo Rosas <lalorosas@outlook.com>
2020-04-16 19:27:42 -0500

    First commit - already with an AppBar

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// This is the main function where all Flutter apps start
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("I Am Rich"),
+            backgroundColor: Colors.blueGrey.shade900,
+          ),
+        ),
+      ),
+    );
