dc870e190099b84474e399b4cb6e9b253e03c90e
Eduardo Rosas <lalorosas@outlook.com>
2020-04-16 20:00:14 -0500

    Adding a centered image in the body

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,16 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
+          backgroundColor: Colors.blueGrey,
           appBar: AppBar(
-            title: Text("I Am Rich"),
+            title: Text('I Am Rich'),
             backgroundColor: Colors.blueGrey.shade900,
+          ),
+          body: Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://www.laingsuk.com/blog/wp-content/uploads/2014/06/DIAMOND.jpg'),
+            ),
           ),
         ),
       ),
